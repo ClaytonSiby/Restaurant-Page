d6026df16f46f0b58bb1fbbe53cb5bd9c99f1f76
Clayton Siby <claytonsiby@gmail.com>
2020-12-06 16:33:26 +0200

    Update package.json to add more scripts useable in dev & production mode

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -56,6 +56,10 @@ const Menu = (() => {
     const priceForItem6 = document.createElement('p');
     const buttonForItem6 = document.createElement('button');
 
+    const appendChildToParent = (parent, child, element) => {
+
+    }
+
     // assigning class attributes to UI elements
     main.classList = 'menu-wrapper';
     mainHeader.classList = 'menu-header';
