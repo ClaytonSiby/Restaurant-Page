2152c114e40cf65b92818fe902e8d1d90a3647ab
Clayton Siby <claytonsiby@gmail.com>
2020-12-06 13:44:43 +0200

    Update Readme.md & add page-screenshot

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # Restaurant-Page
+** The main focus of this project is to build a website (Restaurant Page in this case) that uses webpack and ES6 modules to change the DOM. The pages are static & non-responsive as the main goal was to showcase my skills & gained knowledge working with webpack. Restaurant page has got three different pages, (i.e Home, Menu & Contact). Please take note, the form does not do anything because I haven't added any functionality to it yet.
+## Screenshot
+
+![Screenshot](dist/images/page-screenshot.png)
+
+## Live Demo
+[Live Demo Link](https://arcane-thicket-18116.herokuapp.com/) 
+
+## 🔧 Built with<a name = "with"></a>
+
+HTML, CSS
+JavaScript
+Webpack
+eslint
+stylelint
+
+## Configurations
+- webpack
+- webpack-cli
+- eslint
+- stylelint
+- style-loader
+- css-loader
+
+
+## Prerequisites
+node
+
+## Getting Started
+To install dependencies Run:
+```
+npm install
+```
+
+The webpack.config.js is set up to run consistently as you make changes on the source code, if you do not like this behavior go ahead and remove the `watch: true` on line 9 in the webpack.config.js file.
+
+
+## 🔨 Setup <a name = "setup"></a>
+
+- To clone the repo to your local repository. Follow this steps
+- <code> git clone https://github.com/ClaytonSiby/Restaurant-Page.git</code>
+- <code> cd Restaurant-Page</code>
+
+
+## Author
+
+👤 **Clayton Siby**
+- Github: [@Clayton Siby](https://github.com/ClaytonSiby)
+- Twitter: [@ClaytonSiby](https://twitter.com/ClaytonSiby)
+- Linkedin: [linkedin](https://www.linkedin.com/in/clayton-siby/)
+- claytonsiby@gmail.com
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the fork this repo and create pull request if you want to make changes.
+
+## 👍 Show your support
+
+Give a ⭐️ if you like this project!
+
+## :thumbsup: Acknowledgements
+Let's Build: A Twitter Clone With Ruby on Rails - Parts 1,2 and 3 by WebCrunch
--- src/modules/footer.js
+++ src/modules/footer.js
@@ -1,59 +1,59 @@
 const Footer = (() => {
-    const theFooter = () => {
-      const content = document.getElementById('content');
-      const footer = document.createElement('footer');
-      const rightFooter = document.createElement('div');
-      const leftFooter = document.createElement('div');
-      const midFooter = document.createElement('div');
-      const logo = document.createElement('p');
-      const allRightsReserved = document.createElement('p');
-      const socialLinks = document.createElement('p');
-      const fbLink = document.createElement('span');
-      const linkedIn = document.createElement('span');
-      const twitterLink = document.createElement('span');
-      const icon1 = document.createElement('i');
-      const icon2 = document.createElement('i');
-      const icon3 = document.createElement('i');
+  const theFooter = () => {
+    const content = document.getElementById('content');
+    const footer = document.createElement('footer');
+    const rightFooter = document.createElement('div');
+    const leftFooter = document.createElement('div');
+    const midFooter = document.createElement('div');
+    const logo = document.createElement('p');
+    const allRightsReserved = document.createElement('p');
+    const socialLinks = document.createElement('p');
+    const fbLink = document.createElement('span');
+    const linkedIn = document.createElement('span');
+    const twitterLink = document.createElement('span');
+    const icon1 = document.createElement('i');
+    const icon2 = document.createElement('i');
+    const icon3 = document.createElement('i');
 
-      // add classes to DOM elements
-      footer.classList = 'footer';
-      rightFooter.classList = 'right-footer';
-      leftFooter.classList = 'left-footer';
-      midFooter.classList = 'mid-footer';
-      logo.classList = 'logo';
-      allRightsReserved.classList = 'rights-reserved';
-      socialLinks.classList = 'social-links';
-      fbLink.classList = 'social-link';
-      linkedIn.classList = 'social-link';
-      twitterLink.classList = 'social-link';
-      icon1.classList = 'fab fa-facebook-f';
-      icon2.classList = 'fab fa-linkedin-in';
-      icon3.classList = 'fab fa-twitter';
+    // add classes to DOM elements
+    footer.classList = 'footer';
+    rightFooter.classList = 'right-footer';
+    leftFooter.classList = 'left-footer';
+    midFooter.classList = 'mid-footer';
+    logo.classList = 'logo';
+    allRightsReserved.classList = 'rights-reserved';
+    socialLinks.classList = 'social-links';
+    fbLink.classList = 'social-link';
+    linkedIn.classList = 'social-link';
+    twitterLink.classList = 'social-link';
+    icon1.classList = 'fab fa-facebook-f';
+    icon2.classList = 'fab fa-linkedin-in';
+    icon3.classList = 'fab fa-twitter';
 
-      // add text content to DOM elements
-      logo.textContent = 'SibyRestaurant';
-      allRightsReserved.innerHTML = "&copy; 2020 all rights reserved."
+    // add text content to DOM elements
+    logo.textContent = 'SibyRestaurant';
+    allRightsReserved.innerHTML = '&copy; 2020 all rights reserved.';
 
-      // append child elements to respective parent elements
-      fbLink.appendChild(icon1);
-      linkedIn.appendChild(icon2);
-      twitterLink.appendChild(icon3);
+    // append child elements to respective parent elements
+    fbLink.appendChild(icon1);
+    linkedIn.appendChild(icon2);
+    twitterLink.appendChild(icon3);
 
-      leftFooter.appendChild(logo);
-      midFooter.appendChild(allRightsReserved);
-      rightFooter.appendChild(socialLinks);
+    leftFooter.appendChild(logo);
+    midFooter.appendChild(allRightsReserved);
+    rightFooter.appendChild(socialLinks);
 
-      socialLinks.appendChild(fbLink);
-      socialLinks.appendChild(linkedIn);
-      socialLinks.appendChild(twitterLink);
+    socialLinks.appendChild(fbLink);
+    socialLinks.appendChild(linkedIn);
+    socialLinks.appendChild(twitterLink);
 
-      footer.appendChild(leftFooter);
-      footer.appendChild(midFooter);
-      footer.appendChild(rightFooter);
-      content.appendChild(footer);
-    };
-  
-    return { theFooter }
+    footer.appendChild(leftFooter);
+    footer.appendChild(midFooter);
+    footer.appendChild(rightFooter);
+    content.appendChild(footer);
+  };
+
+  return { theFooter };
 })();
 
 export default Footer;
